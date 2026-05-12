266b05377a19ff21c2e842b43e913c6e63449eb5
Lucas Echeimberg <168256025+Lecheimberg@users.noreply.github.com>
2026-05-11 18:32:02 -0600

    DONE

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade in %:");
+        int percentage = int.Parse(Console.ReadLine());
+
+        string GPA = "";
+        if (percentage >= 90)
+        {
+            GPA = "A";
+        }
+        else if (percentage >= 80)
+        {
+            GPA = "B";
+        }
+        else if (percentage >= 70)
+        {
+            GPA = "C";
+        
+        }
+        else if (percentage >= 60)
+        {
+            GPA = "D";
+        }
+        else
+        {
+            GPA = "F";
+        }
+
+        string sign = "";
+        int lastDigit = percentage % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        if (GPA == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        if (GPA == "F")
+        {
+            sign = " ";
+        }
+        Console.WriteLine($"Your grade is: {GPA}{sign}");
+
+        if (percentage >= 70)
+        {
+        Console.WriteLine("Congrats! You passed the course");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! You'll do better next time.");
+        }
+
     }
 }
